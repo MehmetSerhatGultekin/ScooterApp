40d914d101ecf9197bf4bdbfedb394ab27531202
Serhat Gultekin <104542830+MehmetSerhatGultekin@users.noreply.github.com>
2025-01-08 10:38:28 +0300

    signIn Created

---
+++ ScooterApp/signInScreenVC.swift
@@ -0,0 +1,27 @@
+//
+//  signInScreenVC.swift
+//  ScooterApp
+//
+//  Created by Mehmet Serhat Gültekin on 7.01.2025.
+//
+
+import UIKit
+
+class signInScreenVC: UIViewController {
+    @IBOutlet weak var nameTF: UITextField!
+    @IBOutlet weak var surnameTF: UITextField!
+    @IBOutlet weak var emaillTF: UITextField!
+    @IBOutlet weak var telephoneNoTF: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.navigationItem.setHidesBackButton(true, animated: true)
+        registerButton.layer.cornerRadius = registerButton.frame.height / 2
+    }
+    
+
+  
+
+}
